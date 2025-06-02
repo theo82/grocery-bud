02997eeac998ecb138679ad6aac3770e408502d7
theo82 <theoaristi53@gmail.com>
2025-06-02 16:12:56 +0300

    Grocery Bud - Add Item

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,25 +1,28 @@
  import React, { useState } from 'react'
  
- const Form = () => {
-  const [newItemName, setItemName] = useState('')
+ const Form = ({ addItem }) => {
+   const [newItemName, setNewItemName] = useState('')
 
-  const handleSubmit = (e) => {
-   e.preventDefault();
-   console.log(newItemName);
-   
-  }
+   const handleSubmit = (e) => {
+     e.preventDefault()
+     if (!newItemName) return
+     addItem(newItemName)
+     setNewItemName('')
+   }
    return (
      <form onSubmit={handleSubmit}>
-      <h4>grocery bud</h4>
-      <div className="form-control">
-       <input 
-        type="text" 
-        className='form-input'
-        value={newItemName} 
-        onChange={(event) => setItemName(event.target.value)} 
-      />
-      <button type="submit" className='btn'>add item</button>
-      </div>
+       <h4>grocery bud</h4>
+       <div className='form-control'>
+         <input
+           type='text'
+           className='form-input'
+           value={newItemName}
+           onChange={(event) => setNewItemName(event.target.value)}
+         />
+         <button type='submit' className='btn'>
+           add item
+         </button>
+       </div>
      </form>
    )
  }
