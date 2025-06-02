9767b87a63c2afd9a457c6e2b102578514dffbad
theo82 <theoaristi53@gmail.com>
2025-06-02 16:45:46 +0300

    Grocery Bud - Render Items

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { nanoid } from 'nanoid'
 import Form from './Form'
+import Items from './Items'
 const App = () => {
   const [items, setItems] = useState([])
 
@@ -14,9 +15,12 @@ const App = () => {
     setItems([...items, newItem])
   }
 
+  const removeItem = (itemId) => {}
+
   return (
     <section className='section-center'>
       <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} />
     </section>
   )
 }
---
+++ src/SingleItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleItem = () => {
+  return (
+    <h2>Single Item</h2>
+  )
+}
+
+export default SingleItem
